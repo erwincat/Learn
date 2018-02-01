3379286249e249f98220260978a2c74c72796f3a
wuchangming <erwincat@163.com>
2018-02-01 20:31:58 +0800

    initPage

---
+++ assets/js/layout.js
@@ -0,0 +1,4 @@
+document.addEventListener('load',function(){
+    //网易布局方式，以iphone6为标准，1rem=100px,宽度分为7.5rem;
+    document.documentElement.style.fontSize=document.documentElement.clientWidth/7.5+"px";
+},false);
